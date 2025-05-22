166ac617041c61cbc9852d283151a8463afa74e5
annaZakharchenko <anna.zakharchenko@student.karazin.ua>
2025-05-22 13:53:38 +0200

    course application to admin submettedAt created

--- Coachify.API/obj/Debug/net9.0/Coachify.API.AssemblyInfo.cs
+++ Coachify.API/obj/Debug/net9.0/Coachify.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coachify.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98377555aeff7f344d4ef5162e9e8ef7dffc66ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd80ce7ea322fcadfe4f31fa2a84fb866a27a70e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coachify.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coachify.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
